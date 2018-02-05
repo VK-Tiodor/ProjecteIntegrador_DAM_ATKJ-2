8580788add33230f4845a7a823abb8be9b5a5b92
adriancsbna <adriancsbna@gmail.com>
2018-02-05 13:08:52 +0100

    Android 2

--- DependeciApp/app/src/main/java/dam/android/dependeciapp/MainActivity.java
+++ DependeciApp/app/src/main/java/dam/android/dependeciapp/MainActivity.java
@@ -1,5 +1,6 @@
 package dam.android.dependeciapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
 
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
